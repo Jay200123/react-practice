78d10c6ae5c69e150cff7ec78934bfe4fda53683
Jay200123 <kenken.ayonon@gmail.com>
2023-06-16 19:16:58 +0800

    adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
     return(
         <nav className="navbar">
             <img className="nav-logo" src={icon} alt="Automotive Icon"/>
-            <h3>The Automotive Society of Engineering</h3>
+            <h3>Automotive Society of Engineering</h3>
             <ul>
                 <li><i className="fa fa-home" aria-hidden="true"></i>Home</li>
                 <li><i className="fa fa-info-circle" aria-hidden="true"></i>About</li>
