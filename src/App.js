2cdd34b15f0d32a779a676a63f9d91ea76d8a74b
Jay200123 <kenken.ayonon@gmail.com>
2023-07-01 10:02:02 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -2,44 +2,23 @@ import './styles.css'
 import Navbar from './components/Navbar';
 import Content from './components/Content';
 import Card from './components/Card';
-
-//importing images
-import img1 from "./images/focus.jpg"
-import img2 from "./images/image2.jpg"
-import img3 from "./images/industry.jpg"
-import img4 from "./images/tech.jpg"
-import Footer from './components/Footer';
+import Footer from "./components/Footer"
+import Data from "./data"
 
 function App() {
+
+  const asedata = Data.map((data)=>{
+
+    return <Card header={data.header} text={data.text} images={data.images}/>
+
+  })
+
   return (
     <div>
       <Navbar/>
       <Content/>
       <div className='card-content'>
-
-      <Card
-      images={img1}
-      header="Practical Focus"
-      text="Automotive courses typically have a strong practical component, emphasizing hands-on training and real-world application of knowledge. Students often work with actual vehicles, automotive systems, and tools to gain practical experience."
-      />
-
-      <Card
-      images={img2}
-      header="Specialized Areas"
-      text="Automotive courses cover a broad range of topics related to automobiles, including automotive engineering, design, manufacturing, electronics, diagnostics, and maintenance."
-      />
-
-      <Card
-      images={img3}
-      header="Industry Relevant Skills"
-      text="utomotive courses aim to develop skills that are in demand in the automotive industry. These may include problem-solving, critical thinking, technical skills, communication, teamwork, and project management."
-      />
-
-      <Card
-      images={img4}
-      header="Technological Advancements"
-      text="Automotive technology is continuously evolving, driven by advancements in areas such as electric vehicles, autonomous driving, connected cars, and alternative fuels."
-      />
+        {asedata}
       </div>
       <Footer/>
     </div>
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import img1 from "./images/focus.jpg"
+import img2 from "./images/image2.jpg"
+import img3 from "./images/industry.jpg"
+import img4 from "./images/tech.jpg"
+
+const Data = [
+    {
+        header:"Practical Focus",
+        text:"Automotive courses typically have a strong practical component, emphasizing hands-on training and real-world application of knowledge. Students often work with actual vehicles, automotive systems, and tools to gain practical experience.",
+        images: img1
+    },
+
+    {
+        header:"Specialized Areas",
+        text:"Automotive courses cover a broad range of topics related to automobiles, including automotive engineering, design, manufacturing, electronics, diagnostics, and maintenance.",
+        images:img2
+    },
+
+    {
+        header:"Industry Relevant Skills",
+        text:"Automotive courses aim to develop skills that are in demand in the automotive industry. These may include problem-solving, critical thinking, technical skills, communication, teamwork, and project management.",
+        images: img3
+    },
+
+    {
+        header:"Technological Advancements",
+        text:"Automotive technology is continuously evolving, driven by advancements in areas such as electric vehicles, autonomous driving, connected cars, and alternative fuels.",
+        images: img4
+    }
+]
+
+export default Data
